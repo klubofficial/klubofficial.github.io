0dac8c2372b64e62a39c97dadcf56858f68b82b4
klubofficial <shkim9533@gmail.com>
2026-06-22 13:24:46 +0000

    Update from https://github.com/KWEBofficial/klub-view/commit/cce778b95067e2c8e6e86c0e9ace64f8bff5c844

---
+++ assets/ArrowBack-BYoamIKs.js
@@ -0,0 +1,1 @@
+import{af as r,j as a,ag as t}from"./index-C6Y0IZBl.js";var e={},u=t;Object.defineProperty(e,"__esModule",{value:!0});var o=e.default=void 0,l=u(r()),v=a,d=(0,l.default)((0,v.jsx)("path",{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"}),"ArrowBack");o=e.default=d;export{o as d};
