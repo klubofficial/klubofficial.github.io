ceeec0ab3bd4f197eafec2161ec95b8d46f7496e
klubofficial <shkim9533@gmail.com>
2026-07-06 13:40:46 +0000

    Update from https://github.com/KWEBofficial/klub-view/commit/6aa605b6a26cad4387d2fb5179397475296c5287

--- assets/Delete-Bg8WJvO0.js
+++
@@ -1,1 +0,0 @@
-import{ai as a,j as t,aj as r}from"./index-CsJC10Xh.js";var e={},u=r;Object.defineProperty(e,"__esModule",{value:!0});var l=e.default=void 0,o=u(a()),v=t,i=(0,o.default)((0,v.jsx)("path",{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"}),"Delete");l=e.default=i;export{l as d};
---
+++ assets/index-dIbCJi9h.js
@@ -0,0 +1,13 @@
+import{ai as c,j as e,aj as h,W as x,C as a,b as o,T as s,a4 as u,F as m,a5 as p,B as l,s as r,a_ as j}from"./index-BNfy6MeY.js";import{I as i}from"./InputAdornment-BcBL-hJS.js";var n={},f=h;Object.defineProperty(n,"__esModule",{value:!0});var d=n.default=void 0,g=f(c()),v=e,y=(0,g.default)((0,v.jsx)("path",{d:"M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"}),"SearchTwoTone");d=n.default=y;const S=r(o)(()=>`
+    height: 100%;
+    display: flex;
+    flex: 1;
+    overflow: auto;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`),C=r(j)(({theme:t})=>`
+    background-color: ${t.colors.alpha.white[100]};
+`),T=r(l)(({theme:t})=>`
+    margin-right: -${t.spacing(1)};
+`);function I(){return e.jsxs(e.Fragment,{children:[e.jsx(x,{children:e.jsx("title",{children:"Status - 404"})}),e.jsx(S,{children:e.jsxs(a,{maxWidth:"md",children:[e.jsxs(o,{textAlign:"center",children:[e.jsx("img",{alt:"404",height:180,src:"/static/images/status/404.svg"}),e.jsx(s,{variant:"h2",sx:{my:2},children:"The page you were looking for doesn't exist."}),e.jsx(s,{variant:"h4",color:"text.secondary",fontWeight:"normal",sx:{mb:4},children:"It's on us, we moved the content to a different page. The search below should help!"})]}),e.jsx(a,{maxWidth:"sm",children:e.jsxs(u,{sx:{textAlign:"center",mt:3,p:4},children:[e.jsx(m,{variant:"outlined",fullWidth:!0,children:e.jsx(C,{type:"text",placeholder:"Search terms here...",endAdornment:e.jsx(i,{position:"end",children:e.jsx(T,{variant:"contained",size:"small",children:"Search"})}),startAdornment:e.jsx(i,{position:"start",children:e.jsx(d,{})})})}),e.jsx(p,{sx:{my:4},children:"OR"}),e.jsx(l,{href:"/",variant:"outlined",children:"Go to homepage"})]})})]})})]})}export{I as default};
